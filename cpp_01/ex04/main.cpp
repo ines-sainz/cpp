bbb6967c609efd3393c31f5d8122b03ae8e64bb5
Ines Sainz Ramon <isainz-r@c2r16s3.42madrid.com>
2024-12-18 12:38:46 +0100

    solved exercise 04

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -17,11 +17,11 @@ int main(int argc, char **argv)
 {
 	std::ifstream	toRead;
 	std::ofstream	toWrite;
-	std::string		outFileName = std::string(argv[1]) + std::string(".replace");
+	std::string		outFileName;
 
 	if (argc == 4)
 	{
-		
+		outFileName = std::string(argv[1]) + std::string(".replace");
 		toRead.open(argv[1], std::ios::in);
 		if (!toRead.is_open())
 		{
@@ -38,28 +38,30 @@ int main(int argc, char **argv)
 
 		std::string	lines;
 		size_t		ocurrence;
+		size_t		len_argv_3 = std::string(argv[3]).length();
 		while (std::getline(toRead, lines))
 		{
-			ocurrence = lines.find(argv[2], 0);
-			if (ocurrence == std::string::npos)
-				toWrite << lines;
-			else if (argv[2][0])
+			ocurrence = 0;
+			while (1)
 			{
-				lines.erase(ocurrence, std::string(argv[2]).length());
-				lines.insert(ocurrence, argv[3]);
-				toWrite << lines;
+				ocurrence = lines.find(argv[2], ocurrence);
+				if (ocurrence == std::string::npos)
+					break ;
+				if (argv[2][0])
+				{
+					lines.erase(ocurrence, std::string(argv[2]).length());
+					lines.insert(ocurrence, argv[3]);
+					ocurrence += std::string(argv[3]).length();
+				}
 			}
-			else
-				toWrite << lines;
+			toWrite << lines;
 			if (!toRead.eof())
 				toWrite << std::endl;
 		}
 
 		toRead.close();
 		toWrite.close();
-		std::cout << "ok";
 		return (0);
 	}
-	std::cout << "nop";
 	return (0);
 }
